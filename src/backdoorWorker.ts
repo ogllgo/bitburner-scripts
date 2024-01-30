45acc449f2d8b4062d7a2ae515e29b213a2c093e
gamer <84210906+ogllgo@users.noreply.github.com>
2024-01-30 21:51:20 +1100

    comments
    added some comments about usage

--- src/backdoorWorker.ts
+++ src/backdoorWorker.ts
@@ -1,8 +1,9 @@
 import { NS } from "@ns";
-
+// this is not to be used by humans, you will probably format it wrong (i know i will), 
+// it's a helper script!
 export async function main(ns: NS) {
     for (let i = 0; i < ns.args.length; i++) {
-        ns.singularity.connect(ns.args[i])
+        ns.singularity.connect(ns.args[i]) // connect to every server in args
     }
-    await ns.singularity.installBackdoor()
+    await ns.singularity.installBackdoor() // install a backdoor
 }
